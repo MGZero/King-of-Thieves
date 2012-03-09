7de27ae2e965cb7216854e7eafce4ed23fedba9f
ZFGC <onisephiroth@gmail.com>
2012-03-08 21:13:15 -0500

    Adding graphics

---
+++ King of Thieves/King of Thieves/Graphics/CGraphics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace King_of_Thieves.Graphics
+{
+    static class CGraphics
+    {
+        private static GraphicsDeviceManager _graphicsInfo;
+
+        public static GraphicsDevice GPU
+        {
+            get
+            {
+                return _graphicsInfo.GraphicsDevice;
+            }
+        }
+
+        public static void acquireGraphics(ref GraphicsDeviceManager manager)
+        {
+            _graphicsInfo = manager;
+        }
+
+        
+    }
+}
---
+++ King of Thieves/King of Thieves/Graphics/CRenderable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace King_of_Thieves.Graphics
+{
+    class CRenderable
+    {
+        public CRenderable(Effect shader, params VertexPositionColor[] vertices)
+        {
+            _shader = shader;
+            this.vertices = vertices;
+            _vertexBuffer = new VertexBuffer(CGraphics.GPU, VertexPositionColor.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+        }
+
+        public void draw(GraphicsDevice graphics)
+        {
+
+            foreach (EffectPass pass in _shader.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                CGraphics.GPU.SetVertexBuffer(_vertexBuffer);
+                graphics.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 0, vertices.Length, 0, 1);
+            }
+        }
+
+        public void swapTechnique(string name)
+        {
+            _shader.CurrentTechnique = _shader.Techniques[name];
+        }
+
+        public void swapTechnique(int index)
+        {
+            _shader.CurrentTechnique = _shader.Techniques[index];
+        }
+
+        private Effect _shader;
+        public VertexPositionColor[] vertices;
+        private static VertexBuffer _vertexBuffer;
+    }
+}
